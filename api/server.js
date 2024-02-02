65424d3519954867ace8aa8475e79d8b4b429ec2
omsant02 <omsantoshwar02@gmail.com>
2024-02-02 13:02:25 +0530

    create contract instance using web3js

--- api/server.js
+++ api/server.js
@@ -1,10 +1,17 @@
-// 0xc6d1f49da2f2890ae108efc0a030ec771f3e6231;
-
 const express = require("express");
 const ABI = require("./ABI.json");
 const { Web3 } = require("web3");
 
 const app = express();
+require("dotenv").config();
+
+const MUMBAI_URL = process.env.MUMBAI_URL;
+
+const web3 = new Web3(MUMBAI_URL);
+
+const contractAddress = "0xc6d1f49da2f2890ae108efc0a030ec771f3e6231";
+const contract = new web3.eth.Contract(ABI, contractAddress);
+console.log(contract);
 
 const PORT = 3000;
 app.listen(PORT, () => {
