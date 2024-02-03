505776d99268a220f843ec5d0af2e8f1f1622c70
omsant02 <omsantoshwar02@gmail.com>
2024-02-03 19:21:14 +0530

    Interaction with smart contract(create contract instance)

--- client/src/pages/Wallet.jsx
+++ client/src/pages/Wallet.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Web3 from "web3";
+import ABI from "./ABI.json";
 
 const Wallet = () => {
   const connectWallet = async () => {
@@ -9,7 +10,10 @@ const Wallet = () => {
         const accounts = await window.ethereum.request({
           method: "eth_requestAccounts",
         });
-        console.log(web3, accounts);
+
+        const contractAddress = "0xc6d1f49da2f2890ae108efc0a030ec771f3e6231";
+        const contract = new web3.eth.Contract(ABI, contractAddress);
+        console.log(contract);
       } else {
         throw new Error();
       }
