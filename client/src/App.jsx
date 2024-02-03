b7addf1ffd73f06c1d08dccaf0f2d48f0c36bab2
omsant02 <omsantoshwar02@gmail.com>
2024-02-03 19:16:48 +0530

    connect to metamask

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,13 +5,20 @@ import CreateTask from "./pages/CreateTask";
 import DeleteTask from "./pages/DeleteTask";
 import Navigation from "./pages/Navigation";
 import UpdateTask from "./pages/UpdateTask";
-import ViewAllTask from "./pages/ViewAllTask";
+import ViewAllTasks from "./pages/ViewAllTasks";
 import ViewTask from "./pages/ViewTask";
 import Wallet from "./pages/Wallet";
 import "./App.css";
 
 function App() {
-  const router = createBrowserRouter([{ path: "/", element: <Wallet /> }]);
+  const router = createBrowserRouter([
+    { path: "/", element: <Wallet /> },
+    { path: "/view-all-tasks", element: <ViewAllTasks /> },
+    { path: "/create-task", element: <CreateTask /> },
+    { path: "/view-task", element: <ViewTask /> },
+    { path: "/update-task", element: <UpdateTask /> },
+    { path: "/delete-task", element: <DeleteTask /> },
+  ]);
 
   return (
     <>
--- client/src/pages/Wallet.jsx
+++ client/src/pages/Wallet.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Web3 from "web3";
 
 const Wallet = () => {
+  const connectWallet = async () => {
+    try {
+      if (window.ethereum) {
+        const web3 = new Web3(window.ethereum);
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        console.log(web3, accounts);
+      } else {
+        throw new Error();
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div>
-      <h1 style={{ color: "white" }}>Hi wallet</h1>
-    </div>
+    <>
+      <button onClick={connectWallet}>Connect wallet</button>
+    </>
   );
 };
 
